cb1bc663ed2c298eb1361e726f6ad3ef1999d975
tomaspreira08 <tomaspereira04062008@gmail.com>
2023-12-03 20:14:21 +0000

    dark/light mode com fundos animados e botões de acesso

---
+++ scriptPage2.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleModeButton = document.getElementById("toggleModePage2");
+
+    let isDarkMode = false;
+
+    toggleModeButton.addEventListener("click", function () {
+        isDarkMode = !isDarkMode;
+        updateMode();
+    });
+
+    function updateMode() {
+        const body = document.body;
+        const newMode = isDarkMode ? "dark-mode" : "light-mode";
+        body.className = newMode;
+
+        // Altera a imagem da lâmpada com base no estado do modo claro/escuro
+        lampIcon.src = isDarkMode ? "Imágens/lampada on.png" : "Imágens/lampada off.png";
+
+        // Altera o fundo e a cor do texto do botão com base no estado do modo claro/escuro
+        toggleModeButton.style.backgroundColor = isDarkMode ? "white" : "gray";
+        toggleModeButton.style.color = isDarkMode ? "black" : "white";
+    }
+});
